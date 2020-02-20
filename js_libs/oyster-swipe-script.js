ec0ff343abfa3634ad727c6c2db4973b281be916
agribble <4617133+agribble@users.noreply.github.com>
2020-02-20 13:50:20 -0600

    new version with touch events
    added touch events through touchSwipe.js
    moved main menu button to bottom

---
+++ js_libs/oyster-swipe-script.js
@@ -0,0 +1,40 @@
+var okToSwipeBack = false;
+var okToSwipeNext = false;
+ $(function() {      
+
+      //Enable swiping...
+
+      $("#div_Slide").swipe( {
+
+        //Generic swipe handler for all directions
+
+        swipe:function(event, direction, distance, duration, fingerCount) {
+
+                      console.log("You swiped " + direction );  
+
+                      if( direction === 'right' && okToSwipeBack=== true){
+
+                                          // swipe right - go next
+
+                                         
+		       cpCmndPreviousSlide = 1;
+
+                      } else {
+		 if( direction === 'left' && okToSwipeNext=== true){
+                                          // swipe left - go prev
+
+                                         
+			cpCmndNextSlide = 1;
+		}
+                      }
+ },
+
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+
+         threshold:25
+
+      });
+console.log("touch events enabled for CGEE Oyster");
+    });
+
+  
